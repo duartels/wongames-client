68af96d1cc706a4eb3334c0f48ee7c861cf88b89
Luan Duarte <luansouza789@gmail.com>
2024-03-23 16:12:44 -0300

    banner tests refactor

--- src/components/Banner/test.spec.tsx
+++ src/components/Banner/test.spec.tsx
@@ -4,17 +4,17 @@ import { renderWithTheme } from '@/utils/tests/helpers'
 
 import { Banner } from '.'
 
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death 1',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
 describe('<Banner />', () => {
   it('should render correctly', () => {
-    renderWithTheme(
-      <Banner
-        img="https://source.unsplash.com/user/willianjusten/1042x580"
-        title="Defy death 1"
-        subtitle="<p>Play the new <strong>CrashLands</strong> season</p>"
-        buttonLabel="Buy now"
-        buttonLink="/games/defy-death"
-      />
-    )
+    renderWithTheme(<Banner {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /Defy death 1/i })
