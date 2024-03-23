ba4c464db355e407ff237b3944e26a25ced0b9bb
Luan Duarte <luansouza789@gmail.com>
2024-03-23 14:22:03 -0300

    heading updated

--- src/components/Heading/test.spec.tsx
+++ src/components/Heading/test.spec.tsx
@@ -29,7 +29,7 @@ describe('<Heading />', () => {
   it('should render a heading with a line to the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyle({
-      'border-left': '0.7rem solid #3CD3C1'
+      'border-left': '0.7rem solid #F231A5'
     })
   })
 
@@ -42,5 +42,54 @@ describe('<Heading />', () => {
         modifier: '::after'
       }
     )
+  })
+
+  it('should render a heading with a small size', () => {
+    renderWithTheme(<Heading size="small">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyle({
+      'font-size': '1.6rem'
+    })
+
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyleRule(
+      'width',
+      '3rem',
+      {
+        modifier: '::after'
+      }
+    )
+  })
+
+  it('should render a Heading with a secondary line color', () => {
+    renderWithTheme(
+      <Heading lineLeft lineBottom lineColor="secondary">
+        Won Games
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /Won Games/i })
+
+    expect(heading).toHaveStyle({
+      'border-left': '0.7rem solid #3CD3C1'
+    })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
+      modifier: '::after'
+    })
+  })
+
+  it('should render a Heading with a primary line color', () => {
+    renderWithTheme(
+      <Heading lineLeft lineBottom lineColor="primary">
+        Won Games
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /Won Games/i })
+
+    expect(heading).toHaveStyle({
+      'border-left': '0.7rem solid #F231A5'
+    })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+      modifier: '::after'
+    })
   })
 })
