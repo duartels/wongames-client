51487a5fc3b81831414e158b522f487c6dbfd344
Luan Duarte <luansouza789@gmail.com>
2024-03-23 16:05:29 -0300

    banner created - partial

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Banner } from '.'
+
+export default {
+  title: 'Banner',
+  component: Banner
+} as Meta
+
+export const Default: StoryObj = {
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+}
---
+++ src/components/Banner/test.spec.tsx
@@ -0,0 +1,34 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from '@/utils/tests/helpers'
+
+import { Banner } from '.'
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(
+      <Banner
+        img="https://source.unsplash.com/user/willianjusten/1042x580"
+        title="Defy death 1"
+        subtitle="<p>Play the new <strong>CrashLands</strong> season</p>"
+        buttonLabel="Buy now"
+        buttonLink="/games/defy-death"
+      />
+    )
+
+    expect(
+      screen.getByRole('heading', { name: /Defy death 1/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /defy death 1/i })).toHaveAttribute(
+      'src',
+      'https://source.unsplash.com/user/willianjusten/1042x580'
+    )
+  })
+})
