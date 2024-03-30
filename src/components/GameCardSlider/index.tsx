b3f8d1d8fd8693454489295a6088f0c9b6c89f1f
Luan Duarte <luansouza789@gmail.com>
2024-03-30 15:28:05 -0300

    game slider added

---
+++ src/components/GameCardSlider/index.tsx
@@ -0,0 +1,65 @@
+import {
+  ArrowBackIos as ArrowLeft,
+  ArrowForwardIos as ArrowRight
+} from 'styled-icons/material-outlined'
+
+import { GameCard, GameCardProps } from '../GameCard'
+import { Slider, SliderSettings } from '../Slider'
+import * as S from './styles'
+
+export type GameCardSliderProps = {
+  items: GameCardProps[]
+  color?: 'white' | 'black'
+}
+
+const settings: SliderSettings = {
+  arrows: true,
+  slidesToShow: 4,
+  infinite: false,
+  lazyLoad: 'ondemand',
+  responsive: [
+    {
+      breakpoint: 1375,
+      settings: {
+        slidesToShow: 3.2,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2.2,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        slidesToShow: 1.2,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        slidesToShow: 1.1,
+        arrows: false
+      }
+    }
+  ],
+  nextArrow: <ArrowRight aria-label="next games" />,
+  prevArrow: <ArrowLeft aria-label="previous games" />
+}
+
+export const GameCardSlider = ({
+  items,
+  color = 'black'
+}: GameCardSliderProps) => (
+  <S.Wrapper color={color}>
+    <Slider settings={settings}>
+      {items.map((item, index) => (
+        <GameCard key={index} {...item} />
+      ))}
+    </Slider>
+  </S.Wrapper>
+)
---
+++ src/components/GameCardSlider/stories.tsx
@@ -0,0 +1,58 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { GameCardSlider, GameCardSliderProps } from '.'
+
+export default {
+  title: 'GameCardSlider',
+  component: GameCardSlider
+} as Meta
+
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00'
+  }
+]
+
+export const Default: StoryObj<GameCardSliderProps> = {
+  render: (args) => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+      <GameCardSlider {...args} />
+    </div>
+  ),
+  args: {
+    items
+  },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+}
