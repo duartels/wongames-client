245b173dce5295f1ca057295db40dd0a27f437d6
Luan Duarte <luansouza789@gmail.com>
2024-04-06 15:31:53 -0300

    auth template added

---
+++ src/templates/Auth/index.tsx
@@ -0,0 +1,8 @@
+'use client'
+import * as S from './styles'
+
+export const Auth = () => (
+  <S.Wrapper>
+    <h1>Auth</h1>
+  </S.Wrapper>
+)
---
+++ src/templates/Auth/test.spec.tsx
@@ -0,0 +1,9 @@
+import { renderWithTheme } from '@/utils/tests/helpers'
+
+import { Auth } from '.'
+
+describe('<Auth />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<Auth />)
+  })
+})
