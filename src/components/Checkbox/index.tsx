347dd165cf656d95c1f3d463ab72c3285458a177
Luan Duarte <luansouza789@gmail.com>
2024-04-06 12:20:38 -0300

    checkbox changed to be controlled

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,22 +1,45 @@
+'use client'
+import { InputHTMLAttributes, useState } from 'react'
+
 import * as S from './styles'
 
 type CheckboxProps = {
+  onCheck?: (status: boolean) => void
   label?: string
   labelFor?: string
   labelColor?: 'white' | 'black'
-}
+} & InputHTMLAttributes<HTMLInputElement>
 
 export const Checkbox = ({
+  onCheck,
   label,
   labelFor = '',
-  labelColor = 'white'
-}: CheckboxProps) => (
-  <S.Wrapper>
-    <S.Input type="checkbox" id={labelFor} />
-    {!!label && (
-      <S.Label htmlFor={labelFor} $labelColor={labelColor}>
-        {label}
-      </S.Label>
-    )}
-  </S.Wrapper>
-)
+  labelColor = 'white',
+  ...props
+}: CheckboxProps) => {
+  const [checked, setChecked] = useState(false)
+
+  const onChange = () => {
+    const status = !checked
+    setChecked(status)
+
+    !!onCheck && onCheck(status)
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        type="checkbox"
+        id={labelFor}
+        onChange={onChange}
+        checked={checked}
+        {...props}
+      />
+      {!!label && (
+        <S.Label htmlFor={labelFor} $labelColor={labelColor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
