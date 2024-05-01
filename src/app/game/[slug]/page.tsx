00307df08b1ddef2b6cc8209535350a042a67a96
Luan Duarte <luansouza789@gmail.com>
2024-05-01 14:01:42 -0300

    use static params

--- src/app/game/[slug]/page.tsx
+++ src/app/game/[slug]/page.tsx
@@ -1,5 +1,17 @@
 import { Game as GameTemplate } from '@/templates/Game'
 
-export default function Game() {
-  return <GameTemplate />
+export async function generateStaticParams() {
+  //const posts = await fetch('https://.../posts').then((res) => res.json())
+
+  return [{ slug: 'cyberpunk-2077' }]
+}
+
+export default async function Game() {
+  async function getCover() {
+    return 'https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg'
+  }
+
+  const cover = await getCover()
+
+  return <GameTemplate cover={cover} />
 }
--- src/templates/Game/test.spec.tsx
+++ src/templates/Game/test.spec.tsx
@@ -2,8 +2,11 @@ import { renderWithTheme } from '@/utils/tests/helpers'
 
 import { Game } from '.'
 
+const mockCover =
+  'https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg'
+
 describe('<Game />', () => {
   it('should render the heading', () => {
-    renderWithTheme(<Game />)
+    renderWithTheme(<Game cover={mockCover} />)
   })
 })
