705f12d325be4d359f303dda7bcb446678a31a88
Luan Duarte <luansouza789@gmail.com>
2024-06-10 18:45:41 -0300

    feat: profile menu finished

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -9,24 +9,40 @@ import {
 
 import * as S from './styles'
 
-export const ProfileMenu = () => (
+export type ProfileMenuProps = {
+  activeLink?: '/profile/me' | '/profile/cards' | '/profile/orders'
+}
+
+export const ProfileMenu = ({ activeLink }: ProfileMenuProps) => (
   <S.Nav>
-    <S.StyledLink href="/profile/me">
+    <S.StyledLink
+      href="/profile/me"
+      $isActive={activeLink === '/profile/me'}
+      title="My profile"
+    >
       <AccountCircle size={24} />
       <span>My profile</span>
     </S.StyledLink>
 
-    <S.StyledLink href="/profile/cards">
+    <S.StyledLink
+      href="/profile/cards"
+      $isActive={activeLink === '/profile/cards'}
+      title="My cards"
+    >
       <CreditCard size={24} />
       <span>My cards</span>
     </S.StyledLink>
 
-    <S.StyledLink href="/profile/orders">
+    <S.StyledLink
+      href="/profile/orders"
+      $isActive={activeLink === '/profile/orders'}
+      title="My orders"
+    >
       <FormatListBulleted size={24} />
       <span>My orders</span>
     </S.StyledLink>
 
-    <S.StyledLink href="/logout">
+    <S.StyledLink href="/logout" title="Sign out">
       <ExitToApp size={24} />
       <span>Sign out</span>
     </S.StyledLink>
--- src/components/ProfileMenu/test.spec.tsx
+++ src/components/ProfileMenu/test.spec.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react'
 
+import theme from '@/styles/theme'
 import { renderWithTheme } from '@/utils/tests/helpers'
 
 import { ProfileMenu } from '.'
@@ -15,5 +16,14 @@ describe('<ProfileMenu />', () => {
     expect(screen.getByRole('link', { name: /my cards/i })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /my orders/i })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+  })
+
+  it('should render the menu with an active link defined', () => {
+    renderWithTheme(<ProfileMenu activeLink="/profile/cards" />)
+
+    expect(screen.getByRole('link', { name: /my cards/i })).toHaveStyle({
+      background: theme.colors.primary,
+      color: theme.colors.white
+    })
   })
 })
